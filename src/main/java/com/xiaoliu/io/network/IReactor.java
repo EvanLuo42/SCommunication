07fd6c2fe4df26c566d50a60a993302533811a78
孜然 <2145973003@qq.com>
2020-06-21 15:57:44 +0800

    添加注释

--- src/main/java/com/xiaoliu/io/network/IReactor.java
+++ src/main/java/com/xiaoliu/io/network/IReactor.java
@@ -5,17 +5,24 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 public interface IReactor {
+    //运行(请单线程调用)
     public void run();
 
+    //停止
     public void stop();
 
+    //注册SocketChannel
     public void register(SocketChannel channel);
 
+    //注册ServerSocketChannel
     public void register(ServerSocketChannel channel);
 
+    //启用写监听(不直接调用)
     public void enableWrite(SocketChannel channel);
 
+    //停止写监听(不直接调用)
     public void disableWrite(SocketChannel channel);
 
+    //投递写请求(call me!)
     public void postWrite(SocketChannel channel,ByteBuffer data);
 }
